2841097e958bc5bbf5cb91089a4b31efaf1a81bf
jotarooo <merrylmaitihe@gmail.com>
2021-04-30 13:38:36 -0930

    im a crackhead add des mouvements interpages pour la premiere activite

--- app/src/main/java/com/example/androidtest1/MainActivity.java
+++ app/src/main/java/com/example/androidtest1/MainActivity.java
@@ -2,17 +2,26 @@ package com.example.androidtest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    
+
+    private Button play;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
+    this.play = (Button) findViewById(R.id.button);
+        play.setOnClickListener(view -> {
+            Intent autreact = new Intent (getApplicationContext(), otherActivity.class);
+            startActivity(autreact);
+            finish();
+        });
 
     }
 }
