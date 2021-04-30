cadc9c12e7c18e6cee1fbc0e58047117a3bc297a
jotarooo <merrylmaitihe@gmail.com>
2021-04-30 13:51:03 -0930

    im a crackhead add des mouvements interpages pour la premiere activite retour et entrer

--- app/src/main/java/com/example/androidtest1/otherActivity.java
+++ app/src/main/java/com/example/androidtest1/otherActivity.java
@@ -2,13 +2,27 @@ package com.example.androidtest1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class otherActivity extends AppCompatActivity {
+
+    private Button retour;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_other);
+
+
+        this.retour = (Button) findViewById(R.id.button2);
+        retour.setOnClickListener(view -> {
+            Intent retourArriere = new Intent (getApplicationContext(), MainActivity.class);
+            startActivity(retourArriere);
+            finish();
+        });
+
     }
+
 }
