f08327c64974503488ebe8cf7faa3a10cab7d151
Pothier-Hiva <markizent@gmail.com>
2021-04-30 14:04:44 -1000

    page de démarrage

---
+++ app/src/main/java/com/example/androidtest1/Page_de_demarrage.java
@@ -0,0 +1,14 @@
+package com.example.androidtest1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page_de_demarrage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page_de_demarrage);
+    }
+}
